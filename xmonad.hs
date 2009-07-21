2ba474775554ba0998c57ba7297fcb9e21b5f507
Gaelan D'costa <gaelan@chromakey.wizardry.ca>
2009-07-21 09:08:49 -0400

    Added background task notification for xmonad Mildly pimped zshrc

--- xmonad.hs
+++ xmonad.hs
@@ -2,12 +2,14 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.IM
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.NoBorders
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
 
+import Data.List (isPrefixOf)
 import Data.Ratio ((%))
 
 myWorkspaces = ["1:code", "2:web", "3:IM", "4:fun", "5:mail", "6:xbmc"] ++ map show [7..8] ++ ["9:float"]
@@ -17,8 +19,7 @@ myLayouts = onWorkspace "3:IM" (gridIM (1%7) (Role "buddy_list")) $ onWorkspace 
 -- I want these particular applications on particular workspaces
 myManageHook = composeAll [ className =? "Quodlibet"  --> doShift "4:fun"
                           , className =? "Pidgin"     --> doShift "3:IM"
-                          , className =? "Gran Paradiso" --> doShift "2:web"
-                          , className =? "Iceweasel" --> doShift "2:web"
+                          , className =? "Navigator" --> doShift "2:web"
                           , className =? "Evolution" --> doShift "5:mail"
                           , className =? "MPlayer" --> doFloat
                           , className =? "xbmc.bin" --> doShift "6:xbmc"
@@ -37,7 +38,10 @@ main = do
     , layoutHook = avoidStruts $ myLayouts
     , modMask = mod4Mask -- Rebind Mod to the Windows key
     -- Pipe our statusbar info to Xmonad
-    , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn h }
+    , logHook = dynamicLogWithPP $ xmobarPP
+        { ppOutput = hPutStrLn h
+        , ppUrgent = xmobarColor "yellow" "red" . xmobarStrip
+        }
     -- Hack for Java programs to display properly
     , startupHook = setWMName "LG3D"
     } `additionalKeysP`
@@ -46,3 +50,18 @@ main = do
     , ("M-n", spawn "wicd-client -n")
     ]
   
+-- | Strip xmobar markup. Useful to remove ppHidden color from ppUrgent
+--   field. For example:
+--
+-- >     , ppHidden          = xmobarColor "gray20" "" . wrap "<" ">"
+-- >     , ppUrgent          = xmobarColor "dark orange" "" .  xmobarStrip
+xmobarStrip :: String -> String
+xmobarStrip = strip [] where
+    strip keep x
+      | null x                 = keep
+      | "<fc="  `isPrefixOf` x = strip keep (drop 1 . dropWhile (/= '>') $ x)
+      | "</fc>" `isPrefixOf` x = strip keep (drop 5  x)
+      | '<' == head x          = strip (keep ++ "<") (tail x)
+      | otherwise              = let (good,x') = span (/= '<') x
+                                 in strip (keep ++ good) x'
+
