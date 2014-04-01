e9a4915aac3261290c5619ce876f794f9840a535
Gaelan D'costa <gdcosta@gmail.com>
2014-04-01 10:05:07 -0400

    starter emacs config

---
+++ init.el
@@ -0,0 +1,36 @@
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+(when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
+(load-theme 'cyberpunk t)
+
+(projectile-global-mode)
+
+(add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+				  (smartparens-strict-mode t)
+				  (rainbow-delimiters-mode t)
+				  (turn-on-eldoc-mode)
+				  (rainbow-mode)
+				  (setq mode-name "EL")))
+(add-hook 'ielm-mode-hook (lambda()
+			    (smartparens-strict-mode t)
+			    (rainbow-delimiters-mode t)
+			    (whitespace-mode nil)
+			    (turn-on-eldoc-mode)))
+
+(require 'smartparens-config)
+
+(add-hook 'prog-mode-hook (lambda ()
+			    (smartparens-mode t)))
+(add-hook 'prog-mode-hook 'flycheck-mode)
+
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+(add-hook 'python-mode-hook 'jedi:setup)
+
+(setq inhibit-splash-screen t)
