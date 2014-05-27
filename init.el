44534f62537474cf89d8f6ab1620e5878fc6242c
Gaelan D'costa <gaelan@galloplabs.com>
2014-05-26 20:55:38 -0400

    Display function currently being worked on.

--- init.el
+++ init.el
@@ -28,6 +28,17 @@
 
 (load-theme 'cyberpunk t)
 
+(which-function-mode t)
+
+;; Show the current function name in the header line
+(which-function-mode)
+(setq-default header-line-format
+	      '((which-func-mode ("" which-func-format " "))))
+(setq mode-line-misc-info
+	    ;; We remove Which Function Mode from the mode line, because it's mostly
+	    ;; invisible here anyway.
+	    (assq-delete-all 'which-func-mode mode-line-misc-info))
+
 (projectile-global-mode)
 
 (ido-mode t)
