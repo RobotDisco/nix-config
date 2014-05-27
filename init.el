1fcd33433ecfc18216e0bd83b68547171a226ce3
Gaelan D'costa <gaelan@galloplabs.com>
2014-05-26 20:55:20 -0400

    Fix hang on OS X due to bad dialog modal support.

--- init.el
+++ init.el
@@ -15,6 +15,17 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
+;; Stop the popup dialogs boxes which hang Emacs on OS X
+(when (memq window-system '(mac ns))
+  (defadvice yes-or-no-p (around prevent-dialog activate)
+    "Prevent yes-or-no-p from activating a dialog"
+    (let ((use-dialog-box nil))
+      ad-do-it))
+  (defadvice y-or-n-p (around prevent-dialog-yorn activate)
+    "Prevent y-or-n-p from activating a dialog"
+    (let ((use-dialog-box nil))
+      ad-do-it)))
+
 (load-theme 'cyberpunk t)
 
 (projectile-global-mode)
