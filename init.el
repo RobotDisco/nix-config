cb58888ef1a30c4ee684a49215f2c9a491a3886b
Gaelan D'costa <gaelan@galloplabs.com>
2014-05-26 20:53:46 -0400

    Adding clojure support

--- init.el
+++ init.el
@@ -47,6 +47,29 @@
 
 (add-hook 'python-mode-hook 'jedi:setup)
 
+;; Cider
+(add-hook 'cider-mode-hook 'cider-turn=-on-eldoc-mode)
+(add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+;; Clojure auto-complete
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
+
+;; Clojure auto-complete for tab completion
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+;; Use ac-nrepl insetead of nrepl-doc
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
+
+
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
