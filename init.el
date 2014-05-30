f83ac56050783ec901cafa1aa653a9e2521ae6d4
Gaelan D'costa <gdcosta@gmail.com>
2014-05-29 21:03:42 -0400

    Typo in clojure configuration.

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
 (ac-config-default)
 
 ;; Cider
-(add-hook 'cider-mode-hook 'cider-turn=-on-eldoc-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
