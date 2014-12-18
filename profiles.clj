aeaf340f23893a46598b647198361774672a8fc4
Gaelan D'costa <gdcosta@gmail.com>
2014-12-18 00:43:01 -0500

    Clojure/ssh tweaking.

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,9 @@
-{:user {:plugins [[cider/cider-nrepl "0.6.0"]
-                  [lein-midje "3.1.1"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.8.1"]
+                  [lein-light "0.0.4"]
+                  [lein-midje "3.1.3"]
+		  [lein-ancient "0.5.5"]
+		  [lein-autoreload "0.1.0"]
+		  [lein-checkall "0.1.1"]
+		  [lein-idefiles "0.2.1"]
+		  [lein-plz "0.3.0"
+		  	    :exclusions [[rewrite-clj] [ancient-clj]]]]}}
