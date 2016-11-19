9356084019f24b47aba9b530785544ebeee0bd23
Gaelan D'costa <gdcosta@gmail.com>
2016-11-18 21:44:12 -0500

    Deleting some weird auto-included stuff

--- src/init.el
+++ src/init.el
@@ -187,17 +187,3 @@ point reaches the beginning or end of the buffer, stop there."
 (global-auto-revert-mode)
 
 ;; Random hacks of kindness
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (undo-tree smartparens use-package solarized-theme smex smart-mode-line rainbow-delimiters projectile neotree magit inf-clojure ido-ubiquitous guru-mode ggtags flycheck flx-ido exec-path-from-shell editorconfig company cider ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
