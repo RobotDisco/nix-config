b759704755e297cdf4efee4b2176f0f56cdac1d4
Gaelan D'costa <gdcosta@gmail.com>
2017-08-13 09:42:18 -0400

    new set of standard clojure helpers

--- src/profiles.clj
+++ src/profiles.clj
@@ -1,9 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.1"]
-                  [lein-light "0.0.4"]
-                  [lein-midje "3.1.3"]
-		  [lein-ancient "0.5.5"]
-		  [lein-autoreload "0.1.0"]
-		  [lein-checkall "0.1.1"]
-		  [lein-idefiles "0.2.1"]
-		  [lein-plz "0.3.0"
-		  	    :exclusions [[rewrite-clj] [ancient-clj]]]]}}
+{:user {:plugins [[lein-ancient "0.6.10"]
+                  [jonase/eastwood "0.2.4"]
+                  [lein-kibit "0.1.6-beta2"]]}}
