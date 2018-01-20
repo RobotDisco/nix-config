ac26beacd9d7bd8a70d6403a7cc70ce4d8fafc09
Gaelan D'costa <gdcosta@gmail.com>
2018-01-19 22:21:07 -0500

    Update lein plugins, add bikeshed

--- src/profiles.clj
+++ src/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[lein-ancient "0.6.10"]
-                  [jonase/eastwood "0.2.4"]
-                  [lein-kibit "0.1.6-beta2"]]}}
+{:user {:plugins [[lein-ancient "0.6.15"]
+                  [jonase/eastwood "0.2.5"]
+                  [lein-kibit "0.1.5"]
+                  [lein-bikeshed "0.5.0"]]}}
