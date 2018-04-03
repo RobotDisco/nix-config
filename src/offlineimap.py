42182176d8eb3a57413a32936dba208cdf166ee4
Gaelan D'costa <gdcosta@gmail.com>
2018-04-02 22:09:45 -0400

    Removing OAUTH creds from offlineimap, and revoking.
    Switching to something that decrypts locally stored GPG-encrypted files.

--- src/offlineimap.py
+++ src/offlineimap.py
@@ -1,4 +1,6 @@
 import re
+import os
+import subprocess
 
 def nametrans_local2gmail(folder):
     return {
@@ -23,3 +25,30 @@ def nametrans_gmail2local(folder):
             re.sub('\[Gmail\]\/Sent Mail', 'sent',
             re.sub('\[Gmail\]\/All Mail', 'archive',
             re.sub('INBOX', 'inbox', folder)))))))))
+
+
+def decrypt_client_id():
+    path = "{}/.passwd/offlineimap-oauth2-client-id.gpg".format(os.path.expanduser("~"))
+    args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", path]
+    try:
+        return subprocess.check_output(args).strip()
+    except subprocess.CalledProcessError:
+        return ""
+
+def decrypt_client_secret():
+    path = "{}/.passwd/offlineimap-oauth2-client-secret.gpg".format(os.path.expanduser("~"))
+    args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", path]
+    try:
+        return subprocess.check_output(args).strip()
+    except subprocess.CalledProcessError:
+        return ""
+
+def decrypt_refresh_token(account):
+    account = os.path.basename(account)
+    path = "{}/.passwd/{}-oauth2-refresh-token.gpg".format(os.path.expanduser("~"), account)
+
+    args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", path]
+    try:
+        return subprocess.check_output(args).strip()
+    except subprocess.CalledProcessError:
+        return ""
