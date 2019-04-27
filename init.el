7b838f3ea0e9287d66d5efaff2609bc75d5c12e9
Gaelan D'costa <gdcosta@gmail.com>
2019-04-27 14:55:53 -0400

    Add more intelligence to monitor detection EXWM hook.
    Forgot I have to enable exwm's randr mode, added that to startup.
    Added intelligence to my EXWM hook for monitor changes.
    
    It now:
    1) runs the `xrandr` output and parses it to generate two lists,
    one for connected outputs and one for disconnected.
    2) We loop over the disconnected outputs and disable them via xrandr
    3) We look over connected outputs and enable them via special cases:
    eDP-1) The laptop display is turned on and made primary if it is the *only*
    connected display. Otherwise, it is disabled even if connected.
    DP-1-1) My left-most monitor, is made the primary display if connected.
    DP-1-2) My right-most monitor is activated, set to the right of DP-1-1, and
    rotated left by 90 degrees as I keep it in portrait orientation.
    
    Right now we ignore any other connected monitor. I tried to dynamically detect
    the last previously enabled output so I could just keep appending displays to
    the right, but because lisps are generated in the reverse order of detection,
    this doesn't quite work and I don't know if the extra code is worth it at this
    point. Perhaps one day if I try to connect my laptop to other monitor setups...
    
    I've commented out some handy debugs as well. One day I'll know how to watch
    variables via emacs lisp's debugging mode :)
    
    Also:
    Rename function to gaelan/ instead of gaelan- ... looks nicer? indicates
    namespace more readably?

--- init.el
+++ init.el
@@ -98,23 +98,40 @@
 (exwm-enable)
 
 (require 'exwm-randr)
-(defun gaelan-ewxm-randr-screen-change-hook ()
+(defun gaelan/exwm-randr-screen-change-hook ()
  (let ((xrandr-output-regexp "\n\\([^ ]+\\) \\(dis\\)?connected ")
-       last-output)
+       output-connected
+       output-disconnected)
+       ;; last-output)
    (with-temp-buffer
      (call-process "xrandr" nil t nil)
      (goto-char (point-min))
-     (while (progn
-	      (unless (= (point) (point-min)) (forward-line))
-	      (re-search-forward xrandr-output-regexp nil 'noerror))
-       (if (not (null (match-string 2)))
-	   (call-process "xrandr" nil nil nil " --output" (match-string 1) "--off"))
-       (if (null last-output)
-	   (call-process "xrandr" nil nil nil
-			 "--output" (match-string 1) "--primary" "--auto")
-	 (call-process "xrandr" nil nil nil "--output" (match-string 1) "--auto --right-of" last-output)
-	 (setq last-output (match-string 1)))))))
-(add-hook 'exwm-randr-screen-change-hook 'gaelan-exwm-randr-screen-change-hook)
-
+     (while (re-search-forward xrandr-output-regexp nil 'noerror)
+       (if (null (match-string 2))
+	   (add-to-list 'output-connected (match-string 1))
+	 (add-to-list 'output-disconnected (match-string 1))))
+     ;; (princ output-connected)
+     ;; (princ output-disconnected)
+     (dolist (output output-disconnected)
+       (call-process "xrandr" nil nil nil "--output" output "--off"))
+     (dolist (output output-connected)
+       ;; (princ output)
+       (cond ((string= output "DP-1-1")
+	      (call-process "xrandr" nil nil nil
+			    "--output" output "--primary" "--auto"))
+	     ((string= output "DP-1-2")
+	      (call-process "xrandr" nil nil nil
+			    "--output" output "--auto" "--rotate" "left" "--right-of" "DP-1-1"))
+	     ((string= output "eDP-1")
+	      (if (= (length output-connected) 1)
+		  (call-process "xrandr" nil nil nil
+				"--output" output "--primary" "--auto")
+		(call-process "xrandr" nil nil nil
+			      "--output" output "--off"))))))))
+	     ;; (t (call-process "xrandr" nil nil nil
+	     ;; 		      "--output" output "--auto" "--right-of" last-output)))
+       ;; (setq last-output output)))))
+(add-hook 'exwm-randr-screen-change-hook 'gaelan/exwm-randr-screen-change-hook)
 (setq exwm-randr-workspace-output-plist
       '(1 "DP-1-2"))
+(exwm-randr-enable)
