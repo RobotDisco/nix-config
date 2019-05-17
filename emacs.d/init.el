0376aaa6b8371d1e92d50590e74678fd0e118f8c
Gaelan D'costa <gdcosta@gmail.com>
2019-05-16 23:25:27 -0400

    Emacs tweaks
    Moving config file to be modular.
    Start daemon before creating first X windows frame.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,8 @@
+;;; Store additional config in a 'lisp' subfolder and add it to the load path
+;;; so that 'require' can find the files.
+;;; This must be done before moving `user-emacs-directory'.
+(add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
+
 ;; Load the package manager
 (require 'package)
 
@@ -57,103 +62,6 @@
 	    (org-bullets-mode 1)))
 
 ;; I use Emacs as my window manager
-(require 'exwm)
-(setq exwm-input-global-keys
-      `(
-	;; Bind "s-r" to exit char-mode and fullscreen mode.
-	([?\s-r] . exwm-reset)
-	;; Bind "s-w" to switch workspace interactively.
-	([?\s-w] . exwm-workspace-switch)
-	;; Bind "s-p" to launch applications
-	;; Bind "s-0 to s-9" to switch to a workspace by its index.
-	,@(mapcar (lambda (i)
-		    `(,(kbd (format "s-%d" i)) .
-		      (lambda ()
-			(interactive)
-			(exwm-workspace-switch-create ,i))))
-		  (number-sequence 0 9))))
-;; translate emacs keybindings into CUA-like ones for most apps, since most apps don't observe emacs kebindings and we would like a uniform experience.
-(setq exwm-input-simulation-keys
-      '(;; movement
-	([?\C-b] . [left])
-	([?\M-b] . [C-left])
-	([?\C-f] . [right])
-	([?\M-f] . [C-right])
-	([?\C-p] . [up])
-	([?\C-n] . [down])
-	([?\C-a] . [home])
-	([?\C-e] . [end])
-	([?\M-v] . [prior])
-	([?\C-v] . [next])
-	([?\C-d] . [delete])
-	([?\C-k] . [S-end delete])
-	;; cut/paste
-	([?\C-w] . [?\C-x])
-	([?\M-w] . [?\C-c])
-	([?\C-y] . [?\C-v])
-	;; search
-	([?\C-s] . [?\C-f])))
-
-;; EXWM buffer names should reflect the name of the window of the X application being run.
-(add-hook 'exwm-update-class-hook
-	  (lambda ()
-	    (exwm-workspace-rename-buffer exwm-class-name)))
-
-;; Set some global window management bindings
-(setq exwm-input-global-keys
-      `(
-	;; 's-r': Reset to (line-mode).
-	([?\s-r] . exwm-reset)
-	;; 's-w': Switch workspace.
-	([?\s-w] . exwm-workspace-switch)
-	;; 's-p': Launch application
-	([?\s-p] . (lambda (command)
-		     (interactive (list (read-shell-command "$ ")))
-		     (start-process-shell-command command nil command)))
-	;; 's-<N>': Switch to certain workspace.
-	,@(mapcar (lambda (i)
-		    `(,(kbd (format "s-%d" i)) .
-		      (lambda ()
-			(interactive)
-			(exwm-workspace-switch-create ,i))))
-		  (number-sequence 0 9))))
-(exwm-enable)
-
-(require 'exwm-randr)
-(defun gaelan/exwm-randr-screen-change-hook ()
- (let ((xrandr-output-regexp "\n\\([^ ]+\\) \\(dis\\)?connected ")
-       output-connected
-       output-disconnected)
-       ;; last-output)
-   (with-temp-buffer
-     (call-process "xrandr" nil t nil)
-     (goto-char (point-min))
-     (while (re-search-forward xrandr-output-regexp nil 'noerror)
-       (if (null (match-string 2))
-	   (add-to-list 'output-connected (match-string 1))
-	 (add-to-list 'output-disconnected (match-string 1))))
-     ;; (princ output-connected)
-     ;; (princ output-disconnected)
-     (dolist (output output-disconnected)
-       (call-process "xrandr" nil nil nil "--output" output "--off"))
-     (dolist (output output-connected)
-       ;; (princ output)
-       (cond ((string= output "DP-1-1")
-	      (call-process "xrandr" nil nil nil
-			    "--output" output "--primary" "--auto"))
-	     ((string= output "DP-1-2")
-	      (call-process "xrandr" nil nil nil
-			    "--output" output "--auto" "--rotate" "left" "--right-of" "DP-1-1"))
-	     ((string= output "eDP-1")
-	      (if (= (length output-connected) 1)
-		  (call-process "xrandr" nil nil nil
-				"--output" output "--primary" "--auto")
-		(call-process "xrandr" nil nil nil
-			      "--output" output "--off"))))))))
-	     ;; (t (call-process "xrandr" nil nil nil
-	     ;; 		      "--output" output "--auto" "--right-of" last-output)))
-       ;; (setq last-output output)))))
-(add-hook 'exwm-randr-screen-change-hook 'gaelan/exwm-randr-screen-change-hook)
-(setq exwm-randr-workspace-output-plist
-      '(1 "DP-1-2"))
-(exwm-randr-enable)
+(nconc package-selected-packages '(exwm))
+(eval-after-load 'exwm
+  (require 'gaelan/init-exwm))
