09d6faccf57bbcd83eef8a58c29ca67158aff3a0
Gaelan D'costa <gdcosta@gmail.com>
2019-05-20 12:39:21 -0400

    Add useful cider bindings for equivalent elisp functionality.

--- lisp/gaelan/init-emacs-lisp.el
+++ lisp/gaelan/init-emacs-lisp.el
@@ -11,4 +11,13 @@
 (add-hook 'emacs-lisp-mode-hook #'gaelan/emacs-lisp-mode-hook)
 (add-hook 'ielm-mode-hook #'gaelan/emacs-lisp-mode-hook)
 
+(define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun)
+(define-key emacs-lisp-mode-map (kbd "C-c C-p") 'eval-print-last-sexp)
+(define-key emacs-lisp-mode-map (kbd "C-c C-r") 'eval-region)
+(define-key emacs-lisp-mode-map (kbd "C-c C-k") 'eval-buffer)
+(define-key emacs-lisp-mode-map (kbd "C-c C-l") 'load-file)
+(define-key emacs-lisp-mode-map (kbd "C-c RET") 'macroexpand-1)
+(define-key emacs-lisp-mode-map (kbd "C-c M-m") 'macroexpand-all)
+
+
 (provide 'gaelan/init-emacs-lisp)
