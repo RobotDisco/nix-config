759df35488a296b4ea7d3d8df7b06b46c58aac09
Gaelan D'costa <gdcosta@gmail.com>
2019-07-02 00:33:44 -0400

    My lisp modes are more generic than emacs-lisp specific.
    TODO rename the file to reflect this.

--- emacs.d/lisp/init-emacs-lisp.el
+++ emacs.d/lisp/init-emacs-lisp.el
@@ -3,17 +3,20 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'init-package)
 (require 'init-smartparens)
 (gaelan/require-package 'rainbow-delimiters)
 
-(defun gaelan/emacs-lisp-mode-hook ()
-  "Mode hook when working in Emacs Lisp."
+(defun gaelan/generic-lisp-mode-hook ()
+  "Mode hook when working in any Lisp."
   (smartparens-strict-mode)
   (rainbow-delimiters-mode)
   (turn-on-eldoc-mode))
 
-(add-hook 'emacs-lisp-mode-hook #'gaelan/emacs-lisp-mode-hook)
-(add-hook 'ielm-mode-hook #'gaelan/emacs-lisp-mode-hook)
+(add-hook 'emacs-lisp-mode-hook #'gaelan/generic-lisp-mode-hook)
+(add-hook 'ielm-mode-hook #'gaelan/generic-lisp-mode-hook)
+(add-hook 'clojure-mode-hook #'gaelan/generic-lisp-mode-hook)
+(add-hook 'cider-mode-hook #'gaelan/generic-lisp-mode-hook)
 
 (define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun)
 (define-key emacs-lisp-mode-map (kbd "C-c C-p") 'eval-print-last-sexp)
@@ -23,6 +26,7 @@
 (define-key emacs-lisp-mode-map (kbd "C-c RET") 'macroexpand-1)
 (define-key emacs-lisp-mode-map (kbd "C-c M-m") 'macroexpand-all)
 
+(show-paren-mode)
 
 (provide 'init-emacs-lisp)
 ;;; init-emacs-lisp ends here
