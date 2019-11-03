4d274ae0cd4569b7dcc6ca0040605ec0a0f39a85
Gaelan D'costa <gdcosta@gmail.com>
2019-11-02 21:24:55 -0400

    Move zsh tab completion to its own module

---
+++ zsh/completion.zsh
@@ -0,0 +1,8 @@
+#### FILENAME: completion.zsh
+#### Author: Gaelan D'costa (gdcosta@gmail.com)
+#### LICENSE: https://opensource.org/licenses/GPL-3.0
+####
+#### DESCRIPTION: Configure zshell tab completion
+
+# Enable stock command completion
+autoload -Uz compinit && compinit
