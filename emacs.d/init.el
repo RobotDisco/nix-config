5b80e7cc4d285e1bb3efc3e5d67c4093ab21a096
Gaelan D'costa <gdcosta@gmail.com>
2019-11-03 14:59:42 -0500

    Extract rest of config from init.el into other modules.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,10 +6,6 @@
 ;;; files) and then loads all of those files in tern.
 
 ;;; Code:
-
-;; Produce backtraces when errors occur.
-(setq debug-on-error t)
-
 (let ((minver "26.1"))
   (when (version< emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
@@ -18,8 +14,11 @@
 ;; so that 'require' can find the files.
 ;; This must be done before moving `user-emacs-directory'.
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
 (defconst *is-osx* (eq system-type 'darwin)
   "Is this operating system OSX?")
+(defconst *is-linux* (eq system-type 'gnu/linux)
+  "Is this operating system Linux?")
 
 ;;----------------------------------------------------------------------------
 ;; Adjust garbage collection thresholds during startup, and thereafter
@@ -36,6 +35,7 @@
 ;;----------------------------------------------------------------------------
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
+;; Initialize custom or non-packaged lisp code.
 (require 'init-site-lisp)
 ;; Initialize package manager
 (require 'init-package)
@@ -44,51 +44,16 @@
 (setq user-full-name "Gaelan D'costa"
       user-mail-address "gdcosta@gmail.com")
 
-;; Remove unnecessary chrome.
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(fringe-mode 1)
-
+;; We'll need to load common-lisp functionality, but since we're using macros
+;; we can restrict it to the compliation step to minimize runtime bloat.
 (eval-when-compile
   (require 'cl-lib))
 
-;; Prettify necessary chrome
-(gaelan/require-package 'rebecca-theme)
-(if (daemonp)
-    ;; We need this hack because when you initialize emacs as a daemon,
-    ;; no frame is created so a lot of important theme loading computations
-    ;; do not get run. However, this is especially hacky because we don't
-    ;; want to reload the theme from scratch on every frame creation but
-    ;; that's the only hook we can do this, so our hook has to remove itself
-    ;; when it is done.
-    (cl-labels ((load-my-theme (frame)
-			       (with-selected-frame frame
-				 (load-theme 'rebecca t))
-			       (remove-hook 'after-make-frame-functions #'load-my-theme)))
-      (add-hook 'after-make-frame-functions #'load-my-theme))
-  (load-theme 'rebecca t))
-
-(add-to-list 'default-frame-alist '(font . "Anonymous Pro-14"))
+;; Customize basic Emacs appearance
+(require 'init-appearance)
 
 (when *is-osx*
-    (setq mac-command-modifier 'meta)
-    (setq mac-option-modifier 'super))
-
-;; Handy shortcut for reverting buffers
-(global-set-key (kbd "s-u") 'revert-buffer)
-
-;; It is quicker to type y/n to prompts than "yes" or "no".
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Don't ask about following symlinks, just do it.
-(setq vc-follow-symlinks t)
-
-;; Window systems like OSX should set path based on shell configuration.
-(gaelan/require-package 'exec-path-from-shell)
-(when (require 'exec-path-from-shell nil t)
-  (when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize)))
+    (require 'init-osx))
 
 ;; Helm
 (require 'init-helm)
@@ -103,40 +68,17 @@
 ;; and thus is loaded before this config file runs
 (require 'init-emacs-lisp)
 
-;; Clojure support
-(gaelan/require-package 'cider)
-
-;; Install magit for managing git repos
-(gaelan/require-package 'magit)
-(with-eval-after-load 'magit
-  (global-set-key (kbd "C-x g") 'magit-status)
-  (magit-wip-mode))
-
 ;;; Bitwarden passage manager
 (require 'init-bitwarden)
 
 ;;; Org-mode
 (require 'init-org)
 
-;;; Project Management
-(gaelan/require-packages '(projectile helm-projectile projectile-ripgrep))
-(with-eval-after-load 'helm-projectile
-		      (require 'helm-projectile)
-		      (projectile-mode +1)
-		      (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
-
-;;; Syntax checking
-(with-eval-after-load 'flycheck
-  (add-hook 'after-init-hook #'global-flycheck-mode))
-
-;;; Which key mode
-;;; If we start a key chord, this tells us what actions we can complete via that chord.
-(gaelan/require-package 'which-key)
-(with-eval-after-load 'which-key
-  (which-key-mode))
-
 ;; Window Manager
 (require 'init-exwm)
 
+;; Add miscellaneous config that isn't large enough for its own module.
+(require 'init-misc)
+
 (provide 'init)
-;; init.el ends here
+;;; init.el ends here
--- emacs.d/lisp/init-misc.el
+++ emacs.d/lisp/init-misc.el
@@ -3,15 +3,42 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'init-package)
+
+;; Window systems like OSX should set path based on shell configuration.
+(gaelan/require-package 'exec-path-from-shell)
+(when (require 'exec-path-from-shell nil t)
+  (when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize)))
 
 ;; Handy shortcut for reverting buffers
 (global-set-key (kbd "s-u") 'revert-buffer)
 
-;; It is quicker to type y/n to prompts than "yes" or "no".
-(defalias 'yes-or-no-p 'y-or-n-p)
+;;; Syntax checking
+(with-eval-after-load 'flycheck
+  (add-hook 'after-init-hook #'global-flycheck-mode))
 
-;; Don't ask about following symlinks, just do it.
-(setq vc-follow-symlinks t)
+;;; Which key mode
+;;; If we start a key chord, this tells us what actions we can complete via that chord.
+(gaelan/require-package 'which-key)
+(with-eval-after-load 'which-key
+  (which-key-mode))
+
+;; Clojure support
+(gaelan/require-package 'cider)
+
+;; Install magit for managing git repos
+(gaelan/require-package 'magit)
+(with-eval-after-load 'magit
+  (global-set-key (kbd "C-x g") 'magit-status)
+  (magit-wip-mode))
+
+;;; Project Management
+(gaelan/require-packages '(projectile helm-projectile projectile-ripgrep))
+(with-eval-after-load 'helm-projectile
+		      (require 'helm-projectile)
+		      (projectile-mode +1)
+		      (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 (provide 'init-misc)
 ;;; init-misc.el ends here
