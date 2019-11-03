6eea492fffc507fdd981e9fca7b3492e58c995c6
Gaelan D'costa <gdcosta@gmail.com>
2019-11-02 23:30:55 -0400

    Adding zsh module for user aliases.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,43 @@
+#### FILENAME: aliases.zsh
+#### Author: Gaelan D'costa (gdcosta@gmail.com)
+#### LICENSE: https://opensource.org/licenses/GPL-3.0
+####
+#### DESCRIPTION: User aliases
+#### THANKS: https://github.com/seebi/zshrc for the layout inspiration
+
+### Emacs is my editor
+export EDITOR="emacsclient --alternate-editor=''"
+export VISUAL="$EDITOR -n"
+
+## so use its keybindings in the zsh line editor
+bindkey -e
+
+## If vim doesn't exist, invoke vi instead
+command -v vim --help >/dev/null 2>&1 || alias vim=vi
+
+## Sometimes emacsclient gets stuck, so we need to kill the daemon
+alias killemacs="emacsclient -e '(kill-emacs)'"
+
+### edit common files
+alias aedit=" $EDITOR $ZSH_CONFIG/aliases.zsh; source $ZSH_CONFIG/aliases.zsh"
+alias fedit=" $EDITOR $ZSH_CONFIG/functions.zsh; source $ZSH_CONFIG/functions.zsh"
+alias pedit=" $EDITOR $ZSH_CONFIG/private.zsh; source $ZSH_CONFIG/private.zsh"
+alias eedit=" $EDITOR $HOME/.emacs.d/config.org"
+
+### Optimize standard tools
+## colourize grep
+alias grep='grep --color=auto'
+
+## make ls look nicer
+alias ls=' ls --colour=auto -Fph'
+
+### Tulip workflow
+alias tclone="git clone git@git.internal.tulip.io:"
+alias tdl=" tdocker login"
+
+
+
+
+
+
+
