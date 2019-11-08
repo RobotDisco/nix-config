92cfcdfb6654904b362a97b73464f03393e52016
Gaelan D'costa <gdcosta@gmail.com>
2019-11-08 00:43:19 -0500

    Add treemacs, pairs nicely during project-worlk.

---
+++ lisp/init-treemacs.el
@@ -0,0 +1,11 @@
+;;; init-treemacs.el -- file/project tree navigator. -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+(require 'init-package)
+
+(gaelan/require-packages '(treemacs treemacs-projectile treemacs-magit))
+
+(provide 'init-treemacs)
+;;; init-treemacs.el ends here
