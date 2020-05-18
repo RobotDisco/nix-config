6301e53c7ed66b376ce8eb14fb71469db40d74da
Gaelan D'costa <gdcosta@gmail.com>
2020-05-18 01:29:29 -0400

    Download and include SnapScan 1300i firmware

--- nix/machines/arrakis/configuration.nix
+++ nix/machines/arrakis/configuration.nix
@@ -162,6 +162,17 @@ in
   hardware.cpu.intel.updateMicrocode = true;
 
   hardware.sane.enable = true;
+  nixpkgs.config.sane.extraFirmware = [
+    {
+      src = pkgs.fetchurl {
+        # http://www.openfusion.net/linux/scansnap_1300i
+        url = "http://www.openfusion.net/public/files/1300i_0D12.nal";
+        sha256 = "cbea48c6cee675c2ea970944b49b805d665ee659f753a50b83c176973f507591";
+      };
+      name = "1300i_0D12.nal";
+      backend = "epjitsu";
+    }
+  ];
 
   hardware.trackpoint.enable = true;
   hardware.trackpoint.emulateWheel = true;
