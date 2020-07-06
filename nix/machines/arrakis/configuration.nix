1f25c478919ba4c41795ff8fbf55f76ccf09d6e2
Gaelan D'costa <gdcosta@gmail.com>
2020-07-05 20:22:38 -0400

    Use colemak by default for keyboard layout

--- nix/machines/arrakis/configuration.nix
+++ nix/machines/arrakis/configuration.nix
@@ -93,6 +93,7 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.xkbOptions = "ctrl:nocaps";
+  services.xserver.xkbVariant = "colemak";
   
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
