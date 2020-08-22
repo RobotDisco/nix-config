f188e4bf15d6c1f74661d9059d1d31f5a34df31e
Gaelan D'costa <gdcosta@gmail.com>
2020-08-22 15:22:07 -0400

    Set arrakis' hostname

--- machines/arrakis/configuration.nix
+++ machines/arrakis/configuration.nix
@@ -37,7 +37,7 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = hostName; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
