1dc23f19960a7d86f8f5072c42fe5563672b4947
Gaelan D'costa <gdcosta@gmail.com>
2021-08-05 13:36:57 -0400

    Add other essential work software

--- profiles/tulip.nix
+++ profiles/tulip.nix
@@ -3,8 +3,11 @@
 {
   environment.systemPackages = with pkgs; [
     awscli
+    chefdk
     google-cloud-sdk
+    kubectl
     mattermost-desktop
+    python39Packages.virtualenv
   ];
 
   # We need strongswan support for Tulip's VPN.
