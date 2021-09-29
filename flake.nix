492e35cf6d332b6cb6962d6982be531f6014ae76
Gaelan D'costa <gdcosta@gmail.com>
2021-09-29 16:03:30 -0400

    add darktower config + deploy-rs

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     home-manager.url = "github:nix-community/home-manager/release-21.05";
     darwin.url = "github:lnl7/nix-darwin/master";
+    deploy-rs.url = "github:serokell/deploy-rs";
 
     # Hook up our chosen dependencies to be the ones our other dependencies use
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -14,7 +15,7 @@
   };
 
   # This is where we define our own stuff
-  outputs = { nixpkgs, darwin, emacs-overlay, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, darwin, deploy-rs, emacs-overlay, home-manager, ... }@inputs:
     let
       common-nixos-modules = [
         ./nixos/profiles/common.nix
@@ -43,35 +44,55 @@
 
       {
           
-        nixosConfigurations.arrakis = nixpkgs.lib.nixosSystem
-          {
-            system = "x86_64-linux";
-            modules = common-nixos-modules ++ [
-	            ./nixos/machines/arrakis
-              # The module that loads home-manager
-              home-manager.nixosModules.home-manager
-              ({pkgs, ... }: {
-                # Maybe this will get shared, but for now use an anonymous
-                # nix xpression to create my user
-                users.users.gaelan = {
-                  shell = pkgs.zsh;
-                  isNormalUser = true;
-                  home = "/home/gaelan";
-                  description = "Gaelan D'costa";
-                  # TODO if I have role profiles elsewhere, these should be confingured there
-                  extraGroups = [ "wheel" "networkmanager" "docker" "video" ];
-                };
-              })
-              # My anonymous module that has some (probably oughta be common
-              # settings and my user's customized home-manager config
-	            {
-	              home-manager.useUserPackages = true;
-		            home-manager.useGlobalPkgs = true;
-		            home-manager.users.gaelan =
-      		        import ./home-manager/users/gaelan/default.nix;
-              }
-            ];
+        nixosConfigurations = {
+          arrakis = nixpkgs.lib.nixosSystem
+            {
+              system = "x86_64-linux";
+              modules = common-nixos-modules ++ [
+	              ./nixos/machines/arrakis
+                # The module that loads home-manager
+                home-manager.nixosModules.home-manager
+                ({pkgs, ... }: {
+                  # Maybe this will get shared, but for now use an anonymous
+                  # nix xpression to create my user
+                  users.users.gaelan = {
+                    shell = pkgs.zsh;
+                    isNormalUser = true;
+                    home = "/home/gaelan";
+                    description = "Gaelan D'costa";
+                    # TODO if I have role profiles elsewhere, these should be confingured there
+                    extraGroups = [ "wheel" "networkmanager" "docker" "video" ];
+                  };
+                })
+                # My anonymous module that has some (probably oughta be common
+                # settings and my user's customized home-manager config
+	              {
+	                home-manager.useUserPackages = true;
+		              home-manager.useGlobalPkgs = true;
+		              home-manager.users.gaelan =
+      		          import ./home-manager/users/gaelan/default.nix;
+                }
+              ];
+            };
+          darktower = nixpkgs.lib.nixosSystem
+            {
+              system = "x86_64-linux";
+              modules = common-nixos-modules ++ [
+                ./nixos/machines/darktower
+              ];
+            };
+        };
+
+        deploy.nodes.darktower = {
+          fastConnection = true;
+          sshUser = "gaelan";
+          hostname = "192.168.10.3";
+          profiles.system = {
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.darktower;
           };
+        };
+
+        checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
         darwinConfigurations.caladan = darwin.lib.darwinSystem {
           modules = common-darwin-modules ++ [
---
+++ nixos/machines/darktower/default.nix
@@ -0,0 +1,134 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  # Modules from hardware scan on install
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  # ModeSetting kernel panics when I connect via VNC / Intel AMT
+  boot.kernelParams = [ "nomodeset" ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/7677ff30-29a9-4501-a248-18a480736cbc";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/AAF7-9302";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/9a379088-8979-409f-a2ae-efb3f0de9f23"; }
+      { device = "/dev/disk/by-uuid/4eb298f4-6f32-4f81-9f5c-d317c8c79022"; }
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "darktower"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.eno1.ipv4.addresses = [{
+    address = "192.168.10.3";
+    prefixLength = 24;
+  }];
+  
+  networking.interfaces.enp6s0f0.useDHCP = false;
+  networking.interfaces.enp6s0f1.useDHCP = false;
+
+  networking.defaultGateway = "192.168.10.1";
+  networking.nameservers = [ "192.168.10.1" ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.jane = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  # };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  #   firefox
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
+
