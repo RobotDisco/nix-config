e155258ad89e0e38c5cd63e48bb86de540b1dc3f
Gaelan D'costa <gdcosta@gmail.com>
2021-10-31 15:55:19 -0400

    Give steam pipewire + Intel OpenGL enhancements

--- nixos/machines/arrakis/default.nix
+++ nixos/machines/arrakis/default.nix
@@ -97,4 +97,19 @@ in
   #   SuspendState=disk
   #   SuspendMode=suspend
   # ";
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [ vaapiIntel ];
+  };
 }
--- nixos/profiles/software/steam.nix
+++ nixos/profiles/software/steam.nix
@@ -1,5 +1,8 @@
-# Steam is complicated enough that it needs a bunch of
-# dependencies and system config
+{ config, pkgs, ... }:
+
 {
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux;
+    [ pipewire ];
+
   programs.steam.enable = true;
 }
