6da14630e362da7231e84bf6a598289931d3e4ca
Gaelan D'costa <gdcosta@gmail.com>
2021-11-28 18:11:17 -0500

    Use correct branch for nixpkgs
    I was using the one that maps to the original release, not the brnach
    with any security and bugfixes

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Flakes we're going to depend on
-    nixpkgs.url = github:nixos/nixpkgs/21.05;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-21.05;
     emacs-overlay.url = github:nix-community/emacs-overlay;
     home-manager.url = github:nix-community/home-manager/release-21.05;
     darwin.url = github:lnl7/nix-darwin/master;
