88cf748d8719b3b82fed5da4d4eeea7cc3317525
Gaelan D'costa <gdcosta@gmail.com>
2022-05-21 14:04:34 -0400

    Run nixfmt

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Gaelan's nix-based systems configuration";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-21.11;
-    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -16,16 +16,16 @@
         modules = [
           nixpkgs.nixosModules.notDetected
           nixos-hardware.nixosModules.framework
-	  home-manager.nixosModules.home-manager
+          home-manager.nixosModules.home-manager
           ./nixos/machines/arrakis2022.nix
           {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.gaelan = {
-	      # Set up some reasonble and secure ssh configuration
-	      programs.ssh = {
-		enable = true;
-		compression = true;
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.gaelan = {
+              # Set up some reasonble and secure ssh configuration
+              programs.ssh = {
+                enable = true;
+                compression = true;
                 # Don't forward by default, it is insecure
                 # Prefer proxyjumping if you can
                 forwardAgent = false;
@@ -35,12 +35,8 @@
               programs.git = {
                 enable = true;
                 extraConfig = {
-                  core = {
-                    autocrlf = "input";
-                  };
-                  hub = {
-                    protocol = "https";
-                  };
+                  core = { autocrlf = "input"; };
+                  hub = { protocol = "https"; };
                 };
 
                 # TODO write a tulip nix-shell that sets these to tulip email addresses.
@@ -48,26 +44,27 @@
                 userName = "Gaelan D'costa";
               };
 
-  programs.gpg = {
-    enable = true;
-    settings = {
-      personal-cipher-preferences = "AES256 AES192 AES";
-      personal-digest-preferences = "SHA512 SHA384 SHA256";
-      personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
-      default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
-      cert-digest-algo = "SHA512";
-      s2k-digest-algo = "SHA512";
-      s2k-cipher-algo = "AES256";
-      charset = "utf-8";
-      fixed-list-mode = true;
-      no-comments = true;
-      no-emit-version = true;
-      no-greeting = true;
-      keyid-format = "0xlong";
-      list-options = "show-uid-validity";
-      verify-options = "show-uid-validity";
-      with-fingerprint = true;
-      require-cross-certification = true;
+              programs.gpg = {
+                enable = true;
+                settings = {
+                  personal-cipher-preferences = "AES256 AES192 AES";
+                  personal-digest-preferences = "SHA512 SHA384 SHA256";
+                  personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+                  default-preference-list =
+                    "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+                  cert-digest-algo = "SHA512";
+                  s2k-digest-algo = "SHA512";
+                  s2k-cipher-algo = "AES256";
+                  charset = "utf-8";
+                  fixed-list-mode = true;
+                  no-comments = true;
+                  no-emit-version = true;
+                  no-greeting = true;
+                  keyid-format = "0xlong";
+                  list-options = "show-uid-validity";
+                  verify-options = "show-uid-validity";
+                  with-fingerprint = true;
+                  require-cross-certification = true;
                   no-symkey-cache = true;
                   use-agent = true;
                   throw-keyids = true;
@@ -86,7 +83,7 @@
                 maxCacheTtl = 120;
               };
             };
-	  }
+          }
         ];
       };
     };
--- nixos/machines/arrakis2022.nix
+++ nixos/machines/arrakis2022.nix
@@ -1,12 +1,13 @@
 # My Framework laptop
 { config, pkgs, lib, ... }:
 
-{ 
+{
   networking.hostName = "arrakis";
   networking.interfaces.wlp170s0.useDHCP = true;
   networking.useDHCP = false;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.kernelModules = [ "dm-snapshot" ];
   boot.kernelParams = [ "mem_sleep_default=deep" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -14,39 +15,40 @@
   nix.maxJobs = lib.mkDefault "auto";
   system.stateVersion = "21.11";
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/rootpart0";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/rootpart0";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-label/homepart0";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-label/homepart0";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
 
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-label/nixpart0";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-label/nixpart0";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
 
-  fileSystems."/var" =
-    { device = "/dev/disk/by-label/varpart0";
-      fsType = "ext4";
-      options = [ "relatime" ];
-    };
+  fileSystems."/var" = {
+    device = "/dev/disk/by-label/varpart0";
+    fsType = "ext4";
+    options = [ "relatime" ];
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/EFIBOOT0";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/EFIBOOT0";
+    fsType = "vfat";
+  };
 
-  swapDevices = [ { device = "/dev/disk/by-label/swappart0"; } ];
+  swapDevices = [{ device = "/dev/disk/by-label/swappart0"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
   # high-resolution display
   hardware.video.hidpi.enable = true;
 
@@ -62,9 +64,7 @@
       yubikey = {
         slot = 2;
         twoFactor = true;
-        storage = {
-          device = "/dev/nvme0n1p1";
-        };
+        storage = { device = "/dev/nvme0n1p1"; };
       };
     };
   };
@@ -89,7 +89,7 @@
   environment.variables = {
     GDK_SCALE = "2";
     GDK_DPI_SCALE = "0.5";
-  };                
+  };
 
   services.xserver = {
     enable = true;
@@ -105,21 +105,17 @@
     # Enable nix flakes
     package = pkgs.nixFlakes;
     extraOptions = ''
-        experimental-features = nix-command flakes
+      experimental-features = nix-command flakes
     '';
 
     # Enable binary cache downloads of standard nix packages
-    binaryCaches = [
-      "https://nix-community.cachix.org"
-    ];
+    binaryCaches = [ "https://nix-community.cachix.org" ];
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
-  boot.kernel.sysctl = {
-    "vm.swappiness" = 1;
-  };
+  boot.kernel.sysctl = { "vm.swappiness" = 1; };
 
   services.fstrim.enable = true;
 
@@ -160,9 +156,7 @@
     home = "/home/gaelan";
     description = "Gaelan D'costa";
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keyFiles = [
-      ./gaelan-yubikey.pub
-    ];
+    openssh.authorizedKeys.keyFiles = [ ./gaelan-yubikey.pub ];
   };
 
   services = {
