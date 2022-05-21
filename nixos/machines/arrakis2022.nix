d3d7347f2afb609a9ed4ef432ee1cc7147b5e957
Gaelan D'costa <gdcosta@gmail.com>
2022-05-21 14:02:50 -0400

    Support yubikey for ssh

--- nixos/machines/arrakis2022.nix
+++ nixos/machines/arrakis2022.nix
@@ -164,4 +164,20 @@
       ./gaelan-yubikey.pub
     ];
   };
+
+  services = {
+    # Enable smart card (CCID) mode
+    pcscd.enable = true;
+
+    # udev rules often needed for yubikey support
+    udev.packages = [ pkgs.yubikey-personalization ];
+  };
+
+  # Enable yubikey as a way to login
+  security.pam.yubico = {
+    enable = true;
+    # Use a local challenge-response, not yubico's cloud service
+    mode = "challenge-response";
+    # TODO should I define user "gaelan"'s yubikey here?
+  };
 }
