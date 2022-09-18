510df5492b207bf33b4dd9b7b32e70898ee2dbdb
Gaelan D'costa <gaelan@tulip.com>
2022-09-18 13:37:03 -0400

    Add gaelan user + sshguard

--- nixos/machines/darktower.nix
+++ nixos/machines/darktower.nix
@@ -137,4 +137,19 @@
       ZED_EMAIL_ADDR = [ "gdcosta@gmail.com" ];
     };
   };
+
+  users.users.gaelan = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    home = "/home/gaelan";
+    description = "Gaelan D'costa";
+    extraGroups = [ "wheel" ];
+    # passwordFile = "/run/secrets/users_gaelan_password";
+    # temp password just to get me by
+    initialHashedPassword = "$6$rounds=2500000$PFL/U0wWeXAEL2j$Xf5r7J6quFZYeHtUlNQwjJIPmlZDwS7bRg5u8yWYq2NGF8WLdyiMbK.n1ymvmR3gbT7nDZ4Pdp/MvbOsyOi0E/";
+  };
+
+  nix.trustedUsers = [ "gaelan "];
+
+  services.sshguard.enable = true;
 }
