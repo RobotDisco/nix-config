976ecfdb0419a2be26e3061d8c6b49fff62532b7
Gaelan D'costa <gdcosta@gmail.com>
2022-11-11 23:36:39 -0500

    Make GnuPG home-manager module

--- home-manager/modules/user/gaelan/default.nix
+++ home-manager/modules/user/gaelan/default.nix
@@ -50,45 +50,6 @@ in {
       userName = "Gaelan D'costa";
     };
 
-    programs.gpg = {
-      enable = true;
-      settings = {
-        personal-cipher-preferences = "AES256 AES192 AES";
-        personal-digest-preferences = "SHA512 SHA384 SHA256";
-        personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
-        default-preference-list =
-          "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
-        cert-digest-algo = "SHA512";
-        s2k-digest-algo = "SHA512";
-        s2k-cipher-algo = "AES256";
-        charset = "utf-8";
-        fixed-list-mode = true;
-        no-comments = true;
-        no-emit-version = true;
-        no-greeting = true;
-        keyid-format = "0xlong";
-        list-options = "show-uid-validity";
-        verify-options = "show-uid-validity";
-        with-fingerprint = true;
-        require-cross-certification = true;
-        no-symkey-cache = true;
-        use-agent = true;
-        throw-keyids = true;
-      };
-    };
-
-    services.gpg-agent = {
-      enable = true;
-      enableExtraSocket = true;
-      enableSshSupport = true;
-      extraConfig = ''
-        allow-emacs-pinentry
-        allow-loopback-pinentry
-      '';
-      defaultCacheTtl = 60;
-      maxCacheTtl = 120;
-    };
-
     # Temporarily turn off but should only be on for laptop anyway 
     services.screen-locker = {
       enable = true;
