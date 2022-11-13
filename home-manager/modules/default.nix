171faec66e45fee6394a59e374f09475afd86724
Gaelan D'costa <gdcosta@gmail.com>
2022-11-13 11:02:16 -0500

    Refactor platform-agnostic home-manger config

---
+++ home-manager/modules/default.nix
@@ -0,0 +1,23 @@
+{
+  xdg.enable = true;
+
+  programs.ssh = {
+    enable = true;
+    compression = true;
+    # Don't forward by default, it is insecure
+    # Prefer proxyjumping if you can
+    forwardAgent = false;
+  };
+
+  programs.zsh = {
+    # Since I use zsh, make sure home-manager sets it up.
+    enable = true;
+  };
+
+  home.keyboard = {
+    layout = "us";
+    options = [ "ctrl:nocaps" ];
+  };
+
+  programs.home-manager.enable = true;
+}
--- home-manager/modules/user/gaelan/default.nix
+++ home-manager/modules/user/gaelan/default.nix
@@ -7,8 +7,6 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    xdg.enable = true;
-
     robot-disco.emacs = {
       enable = true;
 
@@ -16,30 +14,10 @@ in {
       enableExwm = true;
     };
 
-    programs.home-manager.enable = true;
-
-    programs.ssh = {
-      enable = true;
-      compression = true;
-      # Don't forward by default, it is insecure
-      # Prefer proxyjumping if you can
-      forwardAgent = false;
-    };
-
-    programs.zsh = {
-      # Since I use zsh, make sure home-manager sets it up.
-      enable = true;
-    };
-
     # Temporarily turn off but should only be on for laptop anyway 
     services.screen-locker = {
       enable = true;
       lockCmd = "${pkgs.i3lock}/bin/i3lock -c 746542";
-    };
-
-    home.keyboard = {
-      layout = "us";
-      options = [ "ctrl:nocaps" ];
     };
 
     home.packages = with pkgs; [
