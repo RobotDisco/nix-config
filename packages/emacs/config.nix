269f70a547ef780f98c55dd034a7b57cb342b76e
Gaelan D'costa <gdcosta@gmail.com>
2022-11-20 13:20:16 -0500

    Port emacs-config packages to nix-config packages

---
+++ packages/emacs/config.nix
@@ -0,0 +1,72 @@
+# This file follows the callPackage pattern
+# see https://nixos.org/guides/nix-pills/callpackage-design-pattern.html
+{ lib, packageRequires, stdenv, trivialBuild, version ? "dev" }:
+
+let
+  # Nix package for emacs lisp which creates
+  # autoloads for all installed emacs packages
+  package-quickstart = trivialBuild {
+    pname = "config-package-quickstart";
+    inherit version packageRequires;
+
+    # We don't need to extract a source archive
+    dontUnpack = true;
+
+    # I think the reason we don't need to explicitly reference an emacs
+    # package is because the trivialBuild function was itself derived from an
+    # emacs package.
+    buildPhase = ''
+      emacs --batch --quick \
+        --load package \
+        --eval '(setq package-quickstart-file "package-quickstart.el")' \
+        --eval '(setq package-quickstart t)' \
+        --funcall package-quickstart-refresh
+    '';
+  };
+
+  # Generate .el from org files, and compile them
+  init = trivialBuild {
+    pname = "config-init";
+    inherit version packageRequires;
+
+    # I guess we need go unpack false because we are including source files?
+    # dontUnpack = false;
+
+    src = lib.sourceByRegex ./. [ "^init.org$" ];
+
+    buildPhase = ''
+      emacs --batch --quick \
+        --load org \
+        *.org \
+        --funcall org-babel-tangle
+
+      # Make a fake config dir because emacs requires it
+      mkdir -p .xdg-config
+      ln -s $PWD .xdg-config/emacs
+      export XDG_CONFIG_HOME="$PWD/.xdg-config"
+
+      # We don't byte compile because a) it may not be portable and b) those
+      # .eln files get stored elsewhere and I don't know how to manage
+      # packaging for that
+
+      emacs -L . --batch --eval '(setq byte-compile-error-on-warn t)' \
+        -f batch-byte-compile *.el
+    '';
+  };
+in stdenv.mkDerivation {
+  pname = "gaelan-emacs-config";
+  inherit version;
+
+  # We don't need to extract a source archive
+  dontUnpack = true;
+
+  # We pass these through just in case anyone needs the derivations
+  # we made to derive our combined and compiled config.
+  passthru.components = { inherit package-quickstart init; };
+
+  installPhase = ''
+    install -D -t $out ${package-quickstart}/share/emacs/site-lisp/*
+    install -D -t $out ${init}/share/emacs/site-lisp/*
+  '';
+}
+
--- packages/emacs/default.nix
+++ packages/emacs/default.nix
@@ -1,16 +1,12 @@
 { pkgs }:
 
+let
+  runtime = import ./runtime.nix { inherit pkgs; };
+in
 {
-  # Emacs package generated from use-package s-expressions in our emacs
-  # configuration file.
-  emacs = pkgs.emacsWithPackagesFromUsePackage {
-    package = pkgs.emacsNativeComp;
-
-    # Parse this org file for "use-package" s-expressions to implicitly import
-    # emacs-overlay nix elisp packages from.
-    config = ./init.org;
-    # Don't assume every "use-package" s-expression should be installed,
-    # respect :ensure keyword.
-    alwaysEnsure = false;
-  };
+  gaelan-emacs = runtime;
+  gaelan-emacs-config = (pkgs.emacsPackagesFor runtime.emacs).callPackage
+    ./config.nix {
+      packageRequires = runtime.explicitRequires;
+    };
 }
