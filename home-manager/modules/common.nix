ae7edaa3f93706b49df77baa9d8fea6c7c724c99
Gaelan D'costa <gdcosta@gmail.com>
2022-11-22 21:05:42 -0500

    Restore mistakenly removed seafile-client package

--- home-manager/modules/common.nix
+++ home-manager/modules/common.nix
@@ -23,5 +23,5 @@
 
   programs.home-manager.enable = true;
 
-  home.packages = with pkgs; [ brave bitwarden calibre discord signal-desktop slack ];
+  home.packages = with pkgs; [ brave bitwarden calibre discord seafile-client signal-desktop slack ];
 }
