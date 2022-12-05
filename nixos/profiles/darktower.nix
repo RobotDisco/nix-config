e7da7def5d7bab0edbb95c14c4ba26efc50a6c73
Gaelan D'costa <gdcosta@gmail.com>
2022-12-05 01:07:03 -0500

    Do upgrade manually, provided script doesn't work

--- nixos/profiles/darktower.nix
+++ nixos/profiles/darktower.nix
@@ -396,29 +396,7 @@
         # For upgrading
         # see https://nixos.org/manual/nixos/stable/index.html#module-services-postgres-upgrading
         environment.systemPackages = [
-          (let
-            newPostgres = pkgs.postgresql_14;
-          in pkgs.writeScriptBin "upgrade-pg-cluster" ''
-            set -eux
-            # XXX stop all services depending on postgresql first.
-            systemctl stop postgresql
-
-            export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
-
-            export NEWBIN="${newPostgres}/bin"
-
-            export OLDDATA="${config.services.postgresql.dataDir}"
-            export OLDBIN="${config.services.postgresql.package}/bin"
-
-            install -d -m 0700 -o postgres -g postgres "$NEWDATA"
-            cd "$NEWDATA"
-            sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
-
-            sudo -u postgres $NEWBIN/pg_upgrade \
-              --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
-              --old-bindir $OLDBIN --new-bindir $NEWBIN \
-            "$@"             
-          '')
+          pkgs.postgresql_14
         ];
       };
     };
