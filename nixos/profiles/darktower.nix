d533f64e3fe8e732209d16c620ab1d1e1a341754
Gaelan D'costa <gdcosta@gmail.com>
2022-12-05 01:00:35 -0500

    Prep for upgrading postgres to 14.x

--- nixos/profiles/darktower.nix
+++ nixos/profiles/darktower.nix
@@ -393,6 +393,35 @@
           location = "/var/backup/postgresql";
           startAt = "*-*-* *:00,15,30,45:00";
         };
+        # For upgrading
+        # see https://nixos.org/manual/nixos/stable/index.html#module-services-postgres-upgrading
+        environment.systemPackages = [
+          (let
+            newPostgresql = pkgs.postgres_14.withPackages (pp: [
+              # Install necessary extensions here.
+            ]);
+          in pkgs.writeScriptBin "upgrade-pg-cluster" ''
+            set -eux
+            # XXX stop all services depending on postgresql first.
+            systemctl stop postgresql
+
+            export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
+
+            export NEWBIN="${newPostgres}/bin"
+
+            export OLDDATA="${config.services.postgresql.dataDir}"
+            export OLDBIN="${config.services.postgresql.package}/bin"
+
+            install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+            cd "$NEWDATA"
+            sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
+
+            sudo -u postgres $NEWBIN/pg_upgrade \
+              --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+              --old-bindir $OLDBIN --new-bindir $NEWBIN \
+            "$@"             
+          '')
+        ];
       };
     };
 
