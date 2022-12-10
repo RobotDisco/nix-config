416244e6ebf16bd2c9897223c750ebc54b850493
Gaelan D'costa <gdcosta@gmail.com>
2022-12-10 17:59:58 -0500

    Upgrade nix flake packages from github by default

--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -1,56 +1,75 @@
 { config, lib, pkgs, ... }:
 
-{
-  # Enable firmware updates
-  services.fwupd.enable = true;
-
-  # Let the root owner manage nix config
-  nix.settings.trusted-users = [ "root" ];
-
-  # Eventually get rid of old nix derivations
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+let cfg = config.robot-disco.common;
+in {
+  options.robot-disco.common.autoUpgrade = {
+    enable = lib.mkOption {
+      description = "Enable system autoupgrades.";
+      default = true;
+      type = lib.types.bool;
+    };
   };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.autoUpgrade.enable {
+      system.autoUpgrade = {
+        enable = true;
+        flake = "github:RobotDisco/nix-config";
 
-  # Try out nsncd instead of ncsd, will become standard in 23.05
-  services.nscd.enableNsncd = true;
+        dates = "daily";
+      };
+    })
+    {
+    # Enable firmware updates
+    services.fwupd.enable = true;
 
-  # Don't allow anyone except those in the admin group to
-  # perform a sudo.
-  security.sudo.execWheelOnly = true;
+    # Let the root owner manage nix config
+    nix.settings.trusted-users = [ "root" ];
 
-  nixpkgs = {
-    # Enable nonfree software
-    config.allowUnfree = true;
-  };
+    # Eventually get rid of old nix derivations
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
 
-  nix = {
-    # Enable nix flakes
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
+    # Try out nsncd instead of ncsd, will become standard in 23.05
+    services.nscd.enableNsncd = true;
 
-  # Enable binary cache downloads of standard nix packages
-  nix.settings.substituters = [ "https://nix-community.cachix.org" ];
-  nix.settings.trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  ];
+    # Don't allow anyone except those in the admin group to
+    # perform a sudo.
+    security.sudo.execWheelOnly = true;
 
-  time.timeZone = "America/Toronto";
-  i18n.defaultLocale = "en_CA.UTF-8";
+    nixpkgs = {
+      # Enable nonfree software
+      config.allowUnfree = true;
+    };
 
-  # Despite it requiring xserver, this is the only way I have found to set
-  # console keyboard layouts
-  console.useXkbConfig = true;
+    nix = {
+      # Enable nix flakes
+      package = pkgs.nixFlakes;
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+    };
 
-  # Enable microcode updates
-  hardware.cpu.intel.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+    # Enable binary cache downloads of standard nix packages
+    nix.settings.substituters = [ "https://nix-community.cachix.org" ];
+    nix.settings.trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
 
-  nix.settings.max-jobs = lib.mkDefault "auto";
-  system.stateVersion = "22.11";
+    time.timeZone = "America/Toronto";
+    i18n.defaultLocale = "en_CA.UTF-8";
+
+    # Despite it requiring xserver, this is the only way I have found to set
+    # console keyboard layouts
+    console.useXkbConfig = true;
+
+    # Enable microcode updates
+    hardware.cpu.intel.updateMicrocode =
+      lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+    nix.settings.max-jobs = lib.mkDefault "auto";
+    system.stateVersion = "22.11";
+  }];
 }
