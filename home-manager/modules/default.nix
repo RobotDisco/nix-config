3b48293b8689210bcd7564b5b2ec26600e8d8b43
Gaelan D'costa <gdcosta@gmail.com>
2022-12-14 00:49:25 -0500

    Add gammastep daytime monitor temperature aligner

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -39,5 +39,17 @@
       signal-desktop
       slack
     ]);
+
+    services.gammastep = lib.mkIf pkgs.stdenv.isLinux {
+      enable = true;
+
+      # My Toronto house
+      # TODO this should be more easily discoverable/configurable.
+      latitude = 43.659720;
+      longitude = -79.429290;
+
+      # TODO do I really want this enabled?
+      tray = true;
+    };
   };
 }
