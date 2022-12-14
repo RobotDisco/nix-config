84f1237aeab9a27225492ba432478fce338fba75
Gaelan D'costa <gdcosta@gmail.com>
2022-12-14 00:48:58 -0500

    Move common.nix into default.nix
    One less file.

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,10 +1,43 @@
+{ lib, pkgs, ... }:
+
 {
   imports = [
-    ./common.nix
     ./development-environment.nix
     ./emacs.nix
     ./games.nix
     ./gnupg.nix
     ./tulip
   ];
+
+  config = {
+    xdg.enable = true;
+
+    programs.ssh = {
+      enable = true;
+      compression = true;
+      # Don't forward by default, it is insecure
+      # Prefer proxyjumping if you can
+      forwardAgent = false;
+    };
+
+    programs.zsh = {
+      # Since I use zsh, make sure home-manager sets it up.
+      enable = true;
+    };
+
+    home.keyboard = {
+      layout = "us";
+      options = [ "ctrl:nocaps" ];
+    };
+
+    home.packages = lib.mkIf pkgs.stdenv.isLinux (with pkgs; [
+      brave
+      bitwarden
+      calibre
+      discord
+      seafile-client
+      signal-desktop
+      slack
+    ]);
+  };
 }
