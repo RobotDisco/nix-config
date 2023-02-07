eb78214051a5ee4286e9c4ed8a5a7bfe4a7cec0a
Gaelan D'costa <gdcosta@gmail.com>
2023-02-06 23:22:32 -0500

    Update linux autorandr config for new docking station

--- nixos/profiles/arrakis2022.nix
+++ nixos/profiles/arrakis2022.nix
@@ -1,3 +1,21 @@
+let
+  displays = {
+    landscape = {
+      device = "DP-1-1-1";
+      fingerprint = "00ffffffffffff0010ac7ba0535347300f1a0104a53420783aee95a3544c99260f5054a1080081408180a940b300d1c0010101010101283c80a070b023403020360006442100001a000000ff004854354e33363446304753530a000000fc0044454c4c2055323431324d0a20000000fd00323d1e5311000a202020202020009f";
+    };
+    portrait = {
+      device = "DP-2-2-1";
+      fingerprint = "00ffffffffffff0010ac7ba04c31543035150104a53420783aee95a3544c99260f5054a1080081408180a940b300d1c0010101010101283c80a070b023403020360006442100001a000000ff004d324743523143533054314c0a000000fc0044454c4c2055323431324d0a20000000fd00323d1e5311000a2020202020200096";
+    };
+    builtin = {
+      device = "eDP-1";
+      fingerprint = "00ffffffffffff0009e55f0900000000171d0104a51c137803de50a3544c99260f505400000001010101010101010101010101010101115cd01881e02d50302036001dbe1000001aa749d01881e02d50302036001dbe1000001a000000fe00424f452043510a202020202020000000fe004e4531333546424d2d4e34310a00fb";
+    };
+  };
+
+in
+
 {
   robot-disco.hardware.framework.enable = true;
   robot-disco.hardware.home-devices.enable = true;
@@ -32,12 +50,9 @@
 
     profiles = {
       default = {
-        fingerprint = {
-          eDP-1 =
-            "00ffffffffffff0009e55f0900000000171d0104a51c137803de50a3544c99260f505400000001010101010101010101010101010101115cd01881e02d50302036001dbe1000001aa749d01881e02d50302036001dbe1000001a000000fe00424f452043510a202020202020000000fe004e4531333546424d2d4e34310a00fb";
-        };
+        fingerprint."${displays.builtin.device}" = displays.builtin.fingerprint;
         config = {
-          eDP-1 = {
+          "${displays.builtin.device}" = {
             enable = true;
             mode = "2256x1504";
             position = "0x0";
@@ -46,20 +61,17 @@
       };
       clamshell-open = {
         fingerprint = {
-          eDP-1 =
-            "00ffffffffffff0009e55f0900000000171d0104a51c137803de50a3544c99260f505400000001010101010101010101010101010101115cd01881e02d50302036001dbe1000001aa749d01881e02d50302036001dbe1000001a000000fe00424f452043510a202020202020000000fe004e4531333546424d2d4e34310a00fb";
-          DP-4-1 =
-            "00ffffffffffff0010ac7ba0535347300f1a0104a53420783aee95a3544c99260f5054a1080081408180a940b300d1c0010101010101283c80a070b023403020360006442100001a000000ff004854354e33363446304753530a000000fc0044454c4c2055323431324d0a20000000fd00323d1e5311000a202020202020009f";
-          DP-4-2 =
-            "00ffffffffffff0010ac7ba04c31543035150104a53420783aee95a3544c99260f5054a1080081408180a940b300d1c0010101010101283c80a070b023403020360006442100001a000000ff004d324743523143533054314c0a000000fc0044454c4c2055323431324d0a20000000fd00323d1e5311000a2020202020200096";
+          "${displays.builtin.device}" = displays.builtin.fingerprint;
+          "${displays.landscape.device}" = displays.landscape.fingerprint;
+          "${displays.portrait.device}" = displays.portrait.fingerprint;
         };
         config = {
-          eDP-1 = {
+          "${displays.builtin.device}" = {
             enable = true;
             mode = "2256x1504";
             position = "0x1504";
           };
-          DP-4-1 = {
+          "${displays.landscape.device}" = {
             enable = true;
             primary = true;
             mode = "1920x1200";
@@ -67,25 +79,23 @@
             scale.x = 1;
             scale.y = 1;
           };
-          DP-4-2 = {
+          "${displays.portrait.device}" = {
             enable = true;
-            mode = "1920x1200";
-            position = "1920x0";
+            mode = "1200x1920";
+            position = "1200x0";
             scale.x = 1;
             scale.y = 1;
-
+            rotate = "left";
           };
         };
       };
       clamshell-closed = {
         fingerprint = {
-          DP-4-1 =
-            "00ffffffffffff0010ac7ba0535347300f1a0104a53420783aee95a3544c99260f5054a1080081408180a940b300d1c0010101010101283c80a070b023403020360006442100001a000000ff004854354e33363446304753530a000000fc0044454c4c2055323431324d0a20000000fd00323d1e5311000a202020202020009f";
-          DP-4-2 =
-            "00ffffffffffff0010ac7ba04c31543035150104a53420783aee95a3544c99260f5054a1080081408180a940b300d1c0010101010101283c80a070b023403020360006442100001a000000ff004d324743523143533054314c0a000000fc0044454c4c2055323431324d0a20000000fd00323d1e5311000a2020202020200096";
+          "${displays.landscape.device}" = displays.landscape.fingerprint;
+          "${displays.portrait.device}" = displays.portrait.fingerprint;
         };
         config = {
-          DP-4-1 = {
+          "${displays.landscape.device}" = {
             enable = true;
             primary = true;
             mode = "1920x1200";
@@ -93,12 +103,13 @@
             scale.x = 1;
             scale.y = 1;
           };
-          DP-4-2 = {
+          "${displays.portrait.device}" = {
             enable = true;
-            mode = "1920x1200";
-            position = "1920x0";
+            mode = "1200x1920";
+            position = "1200x0";
             scale.x = 1;
             scale.y = 1;
+            rotate = "left";
           };
         };
       };
