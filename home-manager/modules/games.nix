aacdb69a59706453ac84f1109f8dd30a76e77900
Gaelan D'costa <gdcosta@gmail.com>
2023-03-27 10:47:01 -0400

    Add support for infocom games + GOG downloads

--- home-manager/modules/games.nix
+++ home-manager/modules/games.nix
@@ -14,7 +14,12 @@ in {
       })
       nethack
       flightgear
+      # Adventure-game engine
       scummvm
+      # Open up GOG artifacts
+      innoextract
+      # Interactive Fiction engine
+      frotz
     ];
   };
 }
