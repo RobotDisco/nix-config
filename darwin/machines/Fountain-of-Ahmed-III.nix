60df325b93ada7dae8bac9e12dc0468c94ca71c0
Gaelan D'costa <gaelan@tulip.com>
2023-07-02 17:52:28 -0400

    Configure Yabai (i3-like window manager) and shkd bindings

--- darwin/machines/Fountain-of-Ahmed-III.nix
+++ darwin/machines/Fountain-of-Ahmed-III.nix
@@ -89,15 +89,47 @@
   };
 
   services.yabai = {
-    enable = false;
+    enable = true;
+    enableScriptingAddition = true;
 
     config = {
-      external_bar = "main";
       mouse_follows_focus = "on";
       # focus_follows_mouse = "autoraise";
       layout = "bsp";
     };
+
+    extraConfig = ''
+function setup_space {
+  local idx="$1"
+  local name="$2"
+  local dsply="$3"
+  local space=
+  echo "setup space $idx: $name"
+
+  space=$(yabai -m query --spaces --space "$idx")
+  if [ -z "$space" ]; then
+    yabai -m space --create
+  fi
+
+  yabai -m space "$idx" --label "$name"
+  yabai -m space "$name" --display "$dsply"
+}
+
+setup_space 1 main 1
+setup_space 2 web 1
+setup_space 3 work 2
+setup_space 4 emacs 2
+setup_space 5 chat 2
+
+yabai -m rule --add app="Brave Browser" space=web
+yabai -m rule --add app="emacs" space=emacs
+yabai -m rule --add app="Mattermost" space=work
+yabai -m rule --add app="Slack" space=chat
+yabai -m rule --add app="Signal" space=chat
+yabai -m rule --add app="WhatsApp" space=chat
+    '';
   };
+
   services.spacebar = {
     enable = false;
     package = pkgs.spacebar;
@@ -111,7 +143,42 @@
       power = "on";
     };
   };
-  services.skhd.enable = true;
+  services.skhd = {
+    enable = true;
+
+    skhdConfig = ''
+alt - q : ${pkgs.yabai}/bin/yabai -m window --close
+alt - w : ${pkgs.yabai}/bin/yabai -m space --layout bsp
+alt - e : ${pkgs.yabai}/bin/yabai -m space --layout float
+
+alt - s : ${pkgs.yabai}/bin/yabai -m space --layout stack
+alt - f : ${pkgs.yabai}/bin/yabai -m window --toggle zoom-fullscreen
+
+# shift + alt - f : ${pkgs.yabai}/bin/yabai -m window --toggle native-fullscreen
+
+alt - h : ${pkgs.yabai}/bin/yabai -m window --focus west
+alt - j : ${pkgs.yabai}/bin/yabai -m window --focus south
+alt - k : ${pkgs.yabai}/bin/yabai -m display --focus next
+alt - l : ${pkgs.yabai}/bin/yabai -m window --focus east
+
+shift + alt - h : ${pkgs.yabai}/bin/yabai -m window --warp west
+shift + alt - j : ${pkgs.yabai}/bin/yabai -m window --warp south
+shift + alt - k : ${pkgs.yabai}/bin/yabai -m window --warp north
+shift + alt - l : ${pkgs.yabai}/bin/yabai -m window --warp east
+
+alt - v : ${pkgs.yabai}/bin/yabai -m window --toggle split
+
+alt - 1 : ${pkgs.yabai}/bin/yabai -m space --focus main
+alt - 2 : ${pkgs.yabai}/bin/yabai -m space --focus web
+alt - 3 : ${pkgs.yabai}/bin/yabai -m space --focus work
+alt - 4 : ${pkgs.yabai}/bin/yabai -m space --focus chat
+
+shift + alt - a : ${pkgs.yabai}/bin/yabai -m display --focus prev
+shift + alt - s : ${pkgs.yabai}/bin/yabai -m space --focus prev
+shift + alt - d : ${pkgs.yabai}/bin/yabai -m space --focus next
+shift + alt - f : ${pkgs.yabai}/bin/yabai -m display --focus next
+    '';
+  };
 
   networking.computerName = "Fountain-of-Ahmed-III";
 }
