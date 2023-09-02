be5fc8d182553ae6503043901acf01ce7d2eca94
Gaelan D'costa <gdcosta@gmail.com>
2023-09-01 23:28:19 -0400

    Add automounted connection to home samba share;
    Note, required credentials are not currently saved in any way.

--- nixos/machines/arrakis2022.nix
+++ nixos/machines/arrakis2022.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 let
   displays = {
     builtin = {
@@ -112,6 +114,24 @@ in
           };
         };
       };
+    };
+
+    fileSystems."/home/gaelan/fileserver" = {
+      device = "//192.168.50.99/archive";
+      fsType = "cifs";
+      options = [
+        # Prevent hanging on network split
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+        # Set samba credentials
+        "credentials=/home/gaelan/smb-secrets"
+        # Mount as user
+        "uid=1000"
+        "gid=100"
+      ];
     };
   };
 }
