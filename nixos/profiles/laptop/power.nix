5487cf6f125606fd9edf4ff0ccc05db216060be4
Gaelan D'costa <gdcosta@gmail.com>
2023-12-24 01:42:23 -0500

    closest thing to reasonable power managwment settings I can come up with.

--- nixos/profiles/laptop/power.nix
+++ nixos/profiles/laptop/power.nix
@@ -13,10 +13,11 @@
   # Temperature management
   services.thermald.enable = true;
 
-  # Set up hybrid sleep on idle and lid close
-  # suspend, then hibernate eventually
+  # Ideally I want to use hybrid-sleep, but for some reason it doesn't work
+  # hibernate, however, does.
+  # so, to preserve my working state during low battery, hibernate on lid close.
   services.logind = {
-    lidSwitch = "hybrid-sleep";
+    lidSwitch = "hibernate";
   };
 
   # Enable powersaving in network manager.
