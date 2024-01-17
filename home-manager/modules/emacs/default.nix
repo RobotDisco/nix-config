bdb304f187b6fb5bcf41fb20ada4b6bddcd465b7
Gaelan D'costa <gdcosta@gmail.com>
2024-01-16 22:18:35 -0500

    Move to emacs 29

--- home-manager/modules/emacs/default.nix
+++ home-manager/modules/emacs/default.nix
@@ -1,6 +1,10 @@
+{ pkgs, ... }:
+
 {
   programs.emacs = {
     enable = true;
+    package = pkgs.emacs29;
+    extraPackages = epkgs: [ epkgs.use-package ];
   };
 
   xdg.configFile."emacs/init.el".source = ./init.el;
