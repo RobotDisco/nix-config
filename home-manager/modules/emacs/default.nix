d63432fa568a71e79a0594b350e8ec02cbe7c28b
Gaelan D'costa <gaelan@tulip.com>
2024-02-05 11:51:45 -0500

    allow emacs to start properly from the shell

--- home-manager/modules/emacs/default.nix
+++ home-manager/modules/emacs/default.nix
@@ -1,13 +1,18 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let emacsPkg = config.programs.emacs.finalPackage;
+in lib.mkMerge [
+  {
+    programs.emacs = {
+      enable = true;
+      package =
+        if pkgs.stdenv.isDarwin then pkgs.emacs-macport else pkgs.emacs-pgtk;
+      extraPackages = epkgs: [ epkgs.use-package ];
+    };
 
-{
-  programs.emacs = {
-    enable = true;
-    package = if pkgs.stdenv.isDarwin
-              then pkgs.emacs-macport
-              else pkgs.emacs-pgtk;
-    extraPackages = epkgs: [ epkgs.use-package ];
-  };
-
-  xdg.configFile."emacs/init.el".source = ./init.el;
-}
+    xdg.configFile."emacs/init.el".source = ./init.el;
+  }
+  (lib.mkIf pkgs.stdenv.isDarwin {
+    programs.zsh.shellAliases.emacs =
+      "${emacsPkg}/Applications/Emacs.app/Contents/MacOS/Emacs";
+  })
+]
--- home-manager/profiles/gaelan-work.nix
+++ home-manager/profiles/gaelan-work.nix
@@ -1,27 +1,27 @@
 let username = "gaelan";
 in {
-imports = [
-  ../modules/emacs
-];
+  imports = [ ../modules/emacs ];
 
-config = {
-  robot-disco.development-environment = {
-    enable = true;
+  config = {
+    robot-disco.development-environment = {
+      enable = true;
 
-    fullname = "Gaelan D'costa";
-    email = "gaelan@tulip.com";
-    gpgKey = "814CF88EBD7287A1!";
+      fullname = "Gaelan D'costa";
+      email = "gaelan@tulip.com";
+      gpgKey = "814CF88EBD7287A1!";
 
-    signCommits = true;
+      signCommits = true;
+    };
+
+    #robot-disco.emacs.enable = true;
+    robot-disco.gnupg.enable = true;
+
+    robot-disco.tulip.enable = true;
+
+    home.stateVersion = "23.11";
+    home.homeDirectory = "/Users/${username}";
+    home.username = username;
+
+    programs.zsh.enable = true;
   };
-
-  #robot-disco.emacs.enable = true;
-  robot-disco.gnupg.enable = true;
-
-  robot-disco.tulip.enable = true;
-
-  home.stateVersion = "23.05";
-  home.homeDirectory = "/Users/${username}";
-  home.username = username;
-};
 }
