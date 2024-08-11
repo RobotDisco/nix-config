ce2710548e254a4ea771ac2091f9ce0fe3602559
Gaelan D'costa <gdcosta@gmail.com>
2024-08-11 17:41:25 -0400

    use flake output for package-specific nix config

--- flake.nix
+++ flake.nix
@@ -170,7 +170,6 @@
       devShells = newLib.forAllSystems (pkgs: {
         default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ git nix nixfmt-rfc-style ];
-          shellHook = "  export NIX_USER_CONF_FILES=${toString ./.}/nix.conf\n";
         };
       });
 
@@ -197,5 +196,20 @@
           {
             brlaser = pkgs.callPackage ./packages/brlaser.nix {};
           };
+
+      nixConfig = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        extra-substituters = [
+          "https://nix-community.cachix.org"
+          "https://robot-disco.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "robot-disco.cachix.org-1:UOaR4+SF1stx8O/Z+bJD9ENNjumfabRNRvCnjwct0sg="
+        ];
+      };
     };
 }
