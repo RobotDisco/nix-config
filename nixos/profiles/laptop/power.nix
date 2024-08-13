3f5abdb4faffe6b2f31409a57bf7c74012895da3
Gaelan D'costa <gdcosta@gmail.com>
2024-08-13 03:56:57 -0400

    Charge laptop when 10%-60%
    Also, try hybrid-sleep again.

--- nixos/profiles/laptop/power.nix
+++ nixos/profiles/laptop/power.nix
@@ -5,19 +5,20 @@
   services.upower = {
     enable = true;
 
-    percentageCritical = 5;
-    percentageAction = 4;
+    percentageLow = 20;
+    percentageCritical = 13;
+    percentageAction = 11;
   };
   # System-level laptop power management (currently AMD prefers power-profile-daemon)
   # services.tlp.enable = true;
   # Temperature management for Intel systems
-  services.thermald.enable = true;
+  # services.thermald.enable = true;
 
   # Ideally I want to use hybrid-sleep, but for some reason it doesn't work
   # hibernate, however, does.
   # so, to preserve my working state during low battery, hibernate on lid close.
   services.logind = {
-    lidSwitch = "hibernate";
+    lidSwitch = "hybrid-sleep";
   };
 
   # Enable powersaving in network manager.
