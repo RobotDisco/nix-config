9b6917b85696310d9b648cd41facaf1133fe2423
Gaelan D'costa <gdcosta@gmail.com>
2024-08-24 15:09:16 -0400

    feat: Tweak power management to be more reliable and power-saving.
    tune low/critical/action actions to shut down at 10%, the internet claims that
    batteries last longest when kept between 10% and 60% total capacity.
    
    Because I'm on an old battery, favour battery savings over fast resume:
    - Hibernate on low battery rather than hybrid-sleep (which stays active even if
      it backs up state to disk.
    - suspend-then-hibernate on laptop close, as if I haven't used my laptop in 20
      minutes I can afford to wait and restart (especially for security/FDE reasons.)

--- modules/nixos/laptop/power.nix
+++ modules/nixos/laptop/power.nix
@@ -1,25 +1,33 @@
 {
   # The daemon that handles things like power actions on lid close, and power
   # actions on low battery.
-  # By default it uses hybrid sleep on critical power actions.
+  # By default it uses hybrid sleep on critical power actions. By the time we
+  # hit critical power I want the machine to hibernate and stay off.
   services.upower = {
     enable = true;
 
-    percentageLow = 20;
-    percentageCritical = 13;
-    percentageAction = 11;
+    criticalPowerAction = "Hibernate";
+
+    percentageLow = 15;
+    percentageCritical = 12;
+    percentageAction = 10;
   };
   # System-level laptop power management (currently AMD prefers power-profile-daemon)
   # services.tlp.enable = true;
   # Temperature management for Intel systems
   # services.thermald.enable = true;
 
-  # Ideally I want to use hybrid-sleep, but for some reason it doesn't work
-  # hibernate, however, does.
-  # so, to preserve my working state during low battery, hibernate on lid close.
+  # hybrid-sleep dumps the memory to disk but stays in a sleep mode until the
+  # battery runs out. This way you will return to your state when charged. Use
+  # if losing power is fine but you want the quickest response.
+  #
+  # suspend-then-hibernate sleeps until a timer runs out and then
+  # hibernates. This allows for a window of waking quickly before preferring
+  # maximum power savings.
   services.logind = {
-    lidSwitch = "hybrid-sleep";
+    lidSwitch = "suspend-then-hibernate";
   };
+  systemd.sleep.extraConfig = "HibernateDelaySec=20m";
 
   # Enable powersaving in network manager.
   networking.networkmanager.wifi.powersave = true;
