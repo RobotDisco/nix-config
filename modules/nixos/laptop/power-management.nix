b37a0d36fc943e84139746c3646282da14d6b50f
Gaelan D'costa <gdcosta@gmail.com>
2024-08-31 09:21:39 -0400

    feat: handle AMD and Intel power management strategies.
    Since Intel and AMD recommend different power management software, configure
    this via a NixOS module option.

--- modules/nixos/laptop/power-management.nix
+++ modules/nixos/laptop/power-management.nix
@@ -1,34 +1,72 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf;
+  cfg = config.robot-disco.power-management;
+in
 {
-  # The daemon that handles things like power actions on lid close, and power
-  # actions on low battery.
-  # By default it uses hybrid sleep on critical power actions. By the time we
-  # hit critical power I want the machine to hibernate and stay off.
-  services.upower = {
-    enable = true;
-
-    criticalPowerAction = "Hibernate";
-
-    percentageLow = 15;
-    percentageCritical = 12;
-    percentageAction = 10;
+  options.robot-disco.power-management = {
+    enableAMD = lib.mkEnableOption "AMD Power Management";
+    enableIntel = lib.mkEnableOption "Intel Power Management";
   };
-  # System-level laptop power management (currently AMD prefers power-profile-daemon)
-  # services.tlp.enable = true;
-  # Temperature management for Intel systems
-  # services.thermald.enable = true;
 
-  # hybrid-sleep dumps the memory to disk but stays in a sleep mode until the
-  # battery runs out. This way you will return to your state when charged. Use
-  # if losing power is fine but you want the quickest response.
-  #
-  # suspend-then-hibernate sleeps until a timer runs out and then
-  # hibernates. This allows for a window of waking quickly before preferring
-  # maximum power savings.
-  services.logind = {
-    lidSwitch = "suspend-then-hibernate";
-  };
-  systemd.sleep.extraConfig = "HibernateDelaySec=20m";
+  config = lib.mkMerge [
+    {
+      # Only one of AMD and Intel Power Management can be enabled.
+      assertions = [
+        {
+          assertion = !(cfg.enableAMD && cfg.enableIntel);
+          message = "Enable only one of 'enableAMD' or 'enableIntel'.";
+        }
+      ];
+    }
+    (mkIf cfg.enableAMD {
+      # Framework / AMD strongly encourage PPD over TLP.
+      services.power-profiles-daemon.enable = true;
 
-  # Enable powersaving in network manager.
-  networking.networkmanager.wifi.powersave = true;
+      services.tlp.enable = false;
+      services.thermald.enable = false;
+    })
+    (mkIf cfg.enableIntel {
+      # TLP is more featureful than TLP, so prefer it for Intel.
+      services.tlp.enable = false;
+      # Temperature management for Intel systems
+      services.thermald.enable = true;
+
+      services.power-profiles-daemon.enable = false;
+    })
+    {
+      # The daemon that desktop environments talk to to get battery
+      # information. It also guides them on what to do when the battery is
+      # critically low.
+      # By default it requests hybrid sleep on critical power actions. By the
+      # time we hit critical power I want the machine to hibernate and stay
+      # off.
+      services.upower = {
+        enable = true;
+
+        criticalPowerAction = "Hibernate";
+
+        percentageLow = 15;
+        percentageCritical = 12;
+        percentageAction = 10;
+      };
+      # Logind governs what happens when the laptop lid is closed.
+      #
+      # hybrid-sleep dumps the memory to disk but stays in a sleep mode until the
+      # battery runs out. This way you will return to your state when charged. Use
+      # if losing power is fine but you want the quickest response.
+      #
+      # suspend-then-hibernate sleeps until a timer runs out and then
+      # hibernates. This allows for a window of waking quickly before preferring
+      # maximum power savings.
+      services.logind = {
+        lidSwitch = "suspend-then-hibernate";
+      };
+      # Sleep for 20 minutes, then hibernate.
+      systemd.sleep.extraConfig = "HibernateDelaySec=20m";
+
+      # Enable wireless powersaving in network manager.
+      networking.networkmanager.wifi.powersave = true;
+    }
+  ];
 }
