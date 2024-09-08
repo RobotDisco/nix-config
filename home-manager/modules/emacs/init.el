43bd0aaea17d2e6b1a14ff18fc8ebd421c1a086c
Gaelan D'costa <gdcosta@gmail.com>
2024-09-07 22:54:24 -0400

    feat: Remove tabnine, I no longer pay for it.

--- home-manager/modules/emacs/init.el
+++ home-manager/modules/emacs/init.el
@@ -537,28 +537,6 @@ If there are no uncompleted todos in the file, remove any :todos: tag."
   :ensure t
   :after magit)
 
-(use-package tabnine
-  :ensure t
-  :commands (tabnine-start-process)
-  :hook (prog-mode . tabnine-mode)
-  :diminish "⌬"
-  :custom
-  (tabnine-wait 1)
-  (tabnine-minimum-prefix-length 0)
-  :hook (kill-emacs . tabnine-kill-process)
-  :config
-  (add-to-list 'completion-at-point-functions #'tabnine-completion-at-point)
-  (tabnine-start-process)
-  :bind
-  (:map  tabnine-completion-map
-	 ("<tab>" . tabnine-accept-completion)
-	 ("TAB" . tabnine-accept-completion)
-	 ("M-f" . tabnine-accept-completion-by-word)
-	 ("M-<return>" . tabnine-accept-completion-by-line)
-	 ("C-g" . tabnine-clear-overlay)
-	 ("M-[" . tabnine-previous-completion)
-	 ("M-]" . tabnine-next-completion)))
-
 (use-package rebecca-theme
   :ensure t
   :config
