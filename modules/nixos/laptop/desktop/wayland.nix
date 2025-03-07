53d6aba614f22bb9b5ec84138b3f0b2377b8187b
Gaelan D'costa <gdcosta@gmail.com>
2025-03-07 10:41:43 -0500

    feat: Use hyprland in greetd

--- modules/nixos/laptop/desktop/wayland.nix
+++ modules/nixos/laptop/desktop/wayland.nix
@@ -17,21 +17,45 @@ let
       color: gold;
     }
   '';
-  swayConfig = pkgs.writeText "greet-sway-config" ''
-    monitor * enable
+  cmpstr = pkgs.hyprland;
+  cmpstrCfg = pkgs.writeText "greet-hypr-config" ''
+    # Disable power-hungry animations
+    decoration {
+      blur {
+        enabled = false
+      }
+      shadow {
+        enabled = false
+      }
+    }
+
+    misc {
+    # Disable hyprland vanity logo and text.
+      disable_hyprland_logo = true
+      disable_splash_rendering = true
+    # Lower frame rate
+      vfr = true
+    }
+
+    # We don't need xwayland for our login screen
+    xwayland {
+      enabled = false
+    }
+
     # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -s ${gtkGreetStyleSheet}; ${pkgs.sway}/bin/swaymsg exit"
-    bindsym Mod4+shift+e exec ${pkgs.sway}/bin/swaynag \
-      -t warning \
-      -m 'Perform an action:' \
-      -b 'Power Down' '${pkgs.systemd}/bin/systemctl poweroff' \
-      -b 'Reboot' '${pkgs.systemd}/bin/systemctl reboot'
+    exec-once = ${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -s ${gtkGreetStyleSheet}; ${cmpstr}/bin/hyprctl exit
   '';
 in
 {
   services.greetd = {
     enable = true;
-    settings.default_session.command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+    settings = {
+      default_session.command = "${cmpstr}/bin/Hyprland --config ${cmpstrCfg}";
+      initial_session = {
+        user = "gaelan";
+        command = "Hyprland";
+      };
+    };
   };
 
   environment.etc."greetd/environments".text = ''
