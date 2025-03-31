105d0fafc103d245f1b9f828227c7cf6a82be35c
Gaelan D'costa <gaelan@tulip.com>
2025-03-31 12:56:52 -0400

    feat: git tweaks from Scott Chacon
    See https://blog.gitbutler.com/how-git-core-devs-configure-git/

--- home-manager/modules/development-environment.nix
+++ home-manager/modules/development-environment.nix
@@ -48,14 +48,47 @@ in
         programs.git = {
           enable = true;
           extraConfig = {
+            # A lot of these are coming from
+            # https://blog.gitbutler.com/how-git-core-devs-configure-git/
+            # as my starting point of understanding git settings.
+            branch = {
+              # Sort by most recent commit date
+              sort = "-comitterdate";
+            };
+            column = {
+              # Put branch names in a column format to save space
+              ui = "auto";
+            };
+            commit = {
+              # Include diff output in the ephemeral part of the commit
+              # editing template.
+              verbose = "true";
+            };
             core = {
               autocrlf = "input";
+              # Configure a global gitignore so you don't have to add files
+              # over and over.
+              excludesfile = "~/.gitignore";
             };
-            hub = {
-              protocol = "https";
+            diff = {
+              # Use a better diffing algorithm.
+              algorithm = "histogram";
+              # Different colour when code is moved
+              # treated differently from add/removed
+              colorMoved = "plain";
+              # Tell me if before/after sections come from index,
+              # working tree, etc...
+              mnemonicPrefix = true;
+              # Visually indicate when files are renamed
+              renames = true;
             };
-            init = {
-              inherit (cfg) defaultBranch;
+            fetch = {
+              # Prune branches locally if the no longer exist remotely.
+              prune = true;
+              # Prune tags locally if the no longer exist remotely.
+              pruneTags = true;
+              # Fetch all removes, don't need to fetch them explicitly.
+              all = true;
             };
             # Configuration for Emacs' Magit Forge package
             # https://magit.vc/manual/forge.html
@@ -64,6 +97,63 @@ in
             };
             gitlab."git.internal.tulip.io" = {
               user = "gaelan";
+            };
+            help = {
+              # If you type in a slightly incorrect command, prompt a revised
+              # suggestion.
+              autocorrect = "prompt";
+            };
+            hub = {
+              protocol = "https";
+            };
+            init = {
+              # Don't let git whine about default branch names, just set it.
+              inherit (cfg) defaultBranch;
+            };
+            merge = {
+              # Show common base code of conflicting commits, YMMV whether this
+              # is useful or not.
+              conflictstyle = "zdiff3";
+            };
+            pull = {
+              # Always do a rebase when pulling.
+              rebase = true;
+            };
+            push = {
+              # Always set up remote branch if it doesn't already exist.
+              autoSetupRemote = true;
+              # By default, only push current branch to same name on remote.
+              default = "simple";
+              # Push local tags automatically to server.
+              followTags = true;
+            };
+            rebase = {
+              # automatically fold fixup commits into original commits when
+              # rebasing.
+              # See `git commit --fixup`, really hand.
+              # https://blog.gitbutler.com/git-autosquash/
+              autoSquash = true;
+              # Automatically stash uncomitted files before rebasing.
+              autoStash = true;
+              # If you have branches of branches, when you do a rebase on a
+              # branch, also update any branches downstream from your
+              # now-rebased branches to also update derived branches.
+              updateRefs = true;
+            };
+            # REuse REcorded REsolutions
+            # This means don't do the same rebase/merge reconciliations over
+            # and over again, redo them if they still apply in later commits
+            # in the rebase/merge operation.
+            rerere = {
+              # Store recorded resolutions
+              enable = true;
+              # Automatically apply them as you go through the list of commits
+              # for the merge/rebase operation.
+              autoupdate = true;
+            };
+            tag = {
+              # Don't sort lexically, understand dot versions.
+              sort = "version:refname";
             };
           };
           lfs.enable = true;
