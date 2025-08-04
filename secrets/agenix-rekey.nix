c1e3346674388ef1cb431764e5fe31786214f31e
Gaelan D'costa <gaelan@tulip.com>
2025-08-04 00:00:38 -0400

    fix: home-manager darwin agenix PATH

--- secrets/agenix-rekey.nix
+++ secrets/agenix-rekey.nix
@@ -2,30 +2,39 @@
   config,
   # osConfig is our NixOS config, when in home-manager
   osConfig ? null,
+  lib,
   pkgs,
   robotdisco-secrets,
   ...
 }:
 
-{
-  age.rekey = {
-
-    agePlugins = [
-      pkgs.age-plugin-yubikey
-    ];
-
-    masterIdentities = [
-      "${robotdisco-secrets}/main/yk-main.pub"
-      "${robotdisco-secrets}/main/yk-backup.pub"
-    ];
-
-    localStorageDir = ../secrets/rekeyed/${
-      if (config ? networking.hostName) then
-        config.networking.hostName
-      else
-        "${osConfig.networking.hostName}-${config.home.username}"
+lib.mkMerge [
+  (lib.mkIf pkgs.stdenv.isDarwin {
+    age = {
+      secretsDir = "$(/usr/bin/getconf DARWIN_USER_TEMP_DIR)/agenix";
+      secretsMountPoint = "$(/usr/bin/getconf DARWIN_USER_TEMP_DIR)/agenix.d";
     };
+  })
+  {
+    age.rekey = {
 
-    storageMode = "local";
-  };
-}
+      agePlugins = [
+        pkgs.age-plugin-yubikey
+      ];
+
+      masterIdentities = [
+        "${robotdisco-secrets}/main/yk-main.pub"
+        "${robotdisco-secrets}/main/yk-backup.pub"
+      ];
+
+      localStorageDir = ../secrets/rekeyed/${
+        if (config ? networking.hostName) then
+          config.networking.hostName
+        else
+          "${osConfig.networking.hostName}-${config.home.username}"
+      };
+
+      storageMode = "local";
+    };
+  }
+]
