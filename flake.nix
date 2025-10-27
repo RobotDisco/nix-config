8083fcd8a40ece73b42d4fee75656fe0ba1d309a
Gaelan D'costa <gdcosta@gmail.com>
2025-10-27 18:31:22 -0400

    feat: remove systemd hibernate hack
    This system hibernate hack has been merged mainline; we don't need to override this anymore.

--- flake.nix
+++ flake.nix
@@ -95,23 +95,6 @@
             ./machines/arrakis
             inputs.agenix.nixosModules.default
             inputs.agenix-rekey.nixosModules.default
-            (
-              {
-                pkgs,
-                ...
-              }:
-              # See https://github.com/systemd/systemd/issues/34304
-              {
-                systemd.package = pkgs.systemd.overrideAttrs (old: {
-                  patches = old.patches ++ [
-                    (pkgs.fetchurl {
-                      url = "https://github.com/wrvsrx/systemd/compare/tag_fix-hibernate-resume%5E...tag_fix-hibernate-resume.patch";
-                      hash = "sha256-Z784xysVUOYXCoTYJDRb3ppGiR8CgwY5CNV8jJSLOXU=";
-                    })
-                  ];
-                });
-              }
-            )
           ];
           specialArgs = {
             inherit myLib;
