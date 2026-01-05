0e6659c2d1a4f8a6e3d12013fa8859d4983823fa
Gaelan D'costa <gdcosta@gmail.com>
2026-01-05 10:15:48 -0500

    fix: use emacs-plus patches, not broken emacs macport
    The macport can't handle invoking a new emacs daemon frame from the commandline

--- home-manager/modules/emacs/package.nix
+++ home-manager/modules/emacs/package.nix
@@ -1,13 +1,35 @@
 {
-  emacsWithPackagesFromUsePackage
-, emacs-macport
-, emacs-pgtk
-, fetchpatch
-, stdenv
+  emacsWithPackagesFromUsePackage,
+  emacs-pgtk,
+  fetchpatch,
+  stdenv,
 }:
 
 let
-  macEmacs = emacs-macport;
+  macEmacs = emacs-pgtk.overrideAttrs (old: {
+    patches = old.patches ++ [
+      # Fix OS window role (needed for window managers like yabai)
+      (fetchpatch {
+        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+        sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+      })
+      # Enable rounded window with no decoration
+      (fetchpatch {
+        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/round-undecorated-frame.patch";
+        sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+      })
+      # Make Emacs aware of OS-level light/dark mode
+      (fetchpatch {
+        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/system-appearance.patch";
+        sha256 = "3QLq91AQ6E921/W9nfDjdOUWR8YVsqBAT/W9c1woqAw=";
+      })
+      # Make Tree-sitter compatible with MacOS
+      (fetchpatch {
+        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/treesit-compatibility.patch";
+        sha256 = "zJHcQ604D7D3pCF+hNfbf8p1xW5490yzrMt1lUsyJQY=";
+      })
+    ];
+  });
 in
 emacsWithPackagesFromUsePackage {
   package = if stdenv.isLinux then emacs-pgtk else macEmacs;
